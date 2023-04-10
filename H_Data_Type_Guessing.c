5d1557cf24211e79d9172ef87e516a148f7722fb
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-04-11 00:53:02 +0600

    cf practice

--- H_Data_Type_Guessing.c
+++ H_Data_Type_Guessing.c
@@ -18,3 +18,25 @@ int main()
     
     return 0;
 }
+
+//---------------youtube---------------------
+
+// #include<stdio.h>
+// int main()
+// {
+//     double a,b,c;
+//     scanf("%lf %lf %lf",&a,&b,&c);
+
+//     double ans1 = (a*b)/c;
+//     long long ans2 = ans1;
+
+//     double result = ans1 - ans2;
+
+//     if(result>0) printf("double\n");
+//     else if(ans2 <=2147483647) printf("int\n");
+//     else printf("long long\n");
+    
+    
+//     return 0;
+// }
+
